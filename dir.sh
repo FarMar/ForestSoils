f09aaa4b094d0eb65b73a767dd8a8c5898fa07e1
Mark Farrell <mark.farrell@csiro.au>
2020-12-02 18:04:33 +1030

    starting BASH

---
+++ dir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#***************************************************************#
+#              makes directory structure                        #
+#***************************************************************#
+
+# should be run from the root of your working directory
+
+mkdir -p data/{raw,working,processed} scripts outputs logs
