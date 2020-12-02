d7c1a869a156a724774d9960a81b9cbdd05f6aaf
Mark Farrell <mark.farrell@csiro.au>
2020-12-02 18:18:46 +1030

    preamble done

---
+++ scripts/ImportSpectra.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#***************************************************************#
+#   Converts folders of Thermo Omnic *.spa files to one *.csv   #
+#																#
+#	Written by mark.farrell@csiro.au							#
+#***************************************************************#
+
+
+
+# Uses a nice ruby script found on https://github.com/ne0dim/spa2csv
+# This script assumes the ruby file is called `spa2csv.rb`, and that
+# the *.spa files are located in the relative path:
+# ../data/raw/MIR
+# It assumes there is a directory called ../data/working to put them in
+# With time I will try to improve it to automate directory checking etc
+
+# Steps as follows
+# 1) Loop the script through a directory/file list of *.spa files - IN PROGRESS
+# 2) Copy the first new .csv and rename it `all_spectra.csv` - NOT DONE
+# 3) Concatenate all the other files, dropping the wavenumbers - NOT DONE
