b0dfb5704e83b78f891036c540dfaa5ca6117b34
Mark Farrell <mark.farrell@csiro.au>
2021-07-29 16:59:17 +0930

    fighting with gradients

--- scripts/inflows.R
+++ scripts/inflows.R
@@ -47,3 +47,18 @@ str(data_long)
 #Create pattern
 gradgreen <- svgpatternsimple::create_pattern_gradient(id="p1", angle=90, colour1="Brown", colour2="Green")
 gradgreen$show()
+
+#encode it
+gradgreen <- svgpatternsimple::encode_pattern_params_as_hex_colour(pattern_name="gradient",angle=90, 
+                                                colour1="Brown", colour2="Green")
+
+ggplot(data_long, aes(x=Age, y=-Year, height=Deaths, group=Year, fill=as.factor(flag)))+
+  geom_density_ridges(stat="identity", alpha=1, scale=3, colour=alpha(0.0001))+
+  theme_classic()+
+  scale_y_continuous(breaks=c(-1993:-2017), labels=c(1993:2017), name="", position="right")+
+  scale_x_discrete(breaks=c("10", "20", "30", "40", "50", "60", "70", "80", "90"))+
+  theme(axis.line.y=element_blank(), axis.ticks.y=element_blank(), text=element_text(family="Georgia"))+
+  labs(title="Trends in deaths from drug poisoning", 
+       subtitle="Data from England and Wales 1993-2017", 
+       caption="Source: Office for National Statistics\nPlot by @VictimOfMaths")+
+  scale_fill_manual(values=c("1"=gradblue), guide=FALSE)
