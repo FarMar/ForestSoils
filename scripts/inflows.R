1e6ac92b2878986db664fa83878e74536f43380f
Mark Farrell <mark.farrell@csiro.au>
2021-07-29 16:36:27 +0930

    gradients starting

--- scripts/inflows.R
+++ scripts/inflows.R
@@ -1,5 +1,6 @@
 ## https://github.com/VictimOfMaths/DeathsOfDespair/blob/master/ONSRidgePlots.R
 install.packages("devtools")
+install.packages("data.table")
 
 devtools::install_github("coolbutuseless/lofi")      # Colour encoding
 devtools::install_github("coolbutuseless/minisvg")   # SVG creation
@@ -8,8 +9,18 @@ devtools::install_github("coolbutuseless/devoutsvg") # This package
 devtools::install_github("coolbutuseless/poissoned") # This package
 devtools::install_github("coolbutuseless/svgpatternsimple") # This package
 
-
 library(data.table)
+library(tidyr)
+library(ggplot2)
+library(ggridges)
+library(dplyr)
+library(lofi)
+library(minisvg)
+library(devout)
+library(devoutsvg)
+library(svgpatternsimple)
+library(poissoned)
+
 
 read.url <- function(url, ...){
   tmpFile <- tempfile()
@@ -27,5 +38,12 @@ data$Age <- case_when(
   TRUE ~ data$Age)
 
 data_long <- melt(as.data.table(data), id.vars = "Age", variable.name = "Year", value.name = "Deaths")
+str(data_long)
 data_long[ , Year := as.integer(as.vector(Year))]
+str(data_long)
 data_long[,flag:=1]
+str(data_long)
+
+#Create pattern
+gradgreen <- svgpatternsimple::create_pattern_gradient(id="p1", angle=90, colour1="Brown", colour2="Green")
+gradgreen$show()
